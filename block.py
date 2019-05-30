5afddc4cae26dc956404edcf89f0c41124086a65
tomcat123a <illogsedywad@126.com>
2019-05-30 16:33:46 -0400

    Update block.py
    with test code

--- block.py
+++ block.py
@@ -8,11 +8,11 @@ Created on Tue May 28 16:08:57 2019
  
 
 class block_edge_conv(torch.nn.Module):#gcn
-    def __init__(self,num_features,hidden_channels=4,out_channels=4,gnn_k=1,gnn_type=2,activation='leaky',res=False,jump=None):
+    def __init__(self,num_features,out_channels=4,gnn_k=1,gnn_type=2,activation='leaky',res=False,jump=None):
         super(block_edge_conv, self).__init__()
         
         self.num_features = num_features    
-        self.hidden_channels=hidden_channels
+        
         self.out_channels=out_channels
         self.gnn_k=gnn_k
         self.gnn_type=gnn_type
@@ -24,7 +24,7 @@ class block_edge_conv(torch.nn.Module):#gcn
             self.activ=F.relu
         if jump=='lstm' and out_channels%2==1 and gnn_k%2==1:
             raise ValueError('out_channels%2==1 and gnn_k%2==1')
-        self.gnn1= GNN(1, self.hidden_channels, self.out_channels, gnn_type=self.gnn_type,gnn_k=self.gnn_k,add_loop=True,jump=jump)
+        self.gnn1= GNN(1, self.out_channels, gnn_type=self.gnn_type,gnn_k=self.gnn_k,add_loop=True,jump=jump)
          
         self.lin1times1 = torch.nn.Linear(self.out_channels,1)
         
@@ -79,14 +79,14 @@ class fc_edge(torch.nn.Module):#gcn
 
 class block_adj_embed(torch.nn.Module):
     #note that for gnn2_pool,the input dimension is the number of hidden nodes after the first pooling operation
-    def __init__(self,num_features,hidden_channels=50,out_channels=50,gnn_k=1,gnn_type=1,activation='leaky',res=False,jump=None\
+    def __init__(self,num_features,out_channels=50,gnn_k=1,gnn_type=1,activation='leaky',res=False,jump=None\
                  ):
         super(block_adj_embed, self).__init__()
         self.num_features = num_features    
          
         self.out_channels=out_channels
          
-        self.hidden_channels=hidden_channels
+        
         if jump=='lstm' and out_channels%2==1 and gnn_k%2==1:
             raise ValueError('out_channels%2==1 and gnn_k%2==1') 
         if gnn_type>=2:
@@ -98,7 +98,7 @@ class block_adj_embed(torch.nn.Module):
         elif activation=='relu':
             self.activ=F.relu
             
-        self.gnn0_embed = GNN(1, hidden_channels, out_channels,gnn_k=gnn_k,gnn_type=gnn_type,add_loop=True,jump=jump)
+        self.gnn0_embed = GNN(1, out_channels,gnn_k=gnn_k,gnn_type=gnn_type,add_loop=True,jump=jump)
         self.lin0_1times1 = torch.nn.Linear(self.out_channels,1)
         
     def forward(self, x_input, adj, mask=None):
@@ -108,14 +108,14 @@ class block_adj_embed(torch.nn.Module):
     
 class block_adj_pool(torch.nn.Module):
     #note that for gnn2_pool,the input dimension is the number of hidden nodes after the first pooling operation
-    def __init__(self,num_features,hidden_channels=10,out_channels=10,out_clusters=20,gnn_k=1,gnn_type=1,activation='leaky',res=False,jump=None\
+    def __init__(self,num_features,out_channels=10,out_clusters=20,gnn_k=1,gnn_type=1,activation='leaky',res=False,jump=None\
                  ,shrink_factor=0.2):
         super(block_adj_pool, self).__init__()
         self.num_features = num_features    
         self.out_clusters=out_clusters
         self.out_channels=out_channels
         self.sh_f=shrink_factor
-        self.hidden_channels=hidden_channels
+       
         assert out_clusters*shrink_factor>=2
         if gnn_type>=2:
             raise ValueError('gnn_type must be 0 or 1') 
@@ -127,8 +127,8 @@ class block_adj_pool(torch.nn.Module):
             self.activ=F.relu
         if jump=='lstm' and out_channels%2==1 and gnn_k%2==1:
             raise ValueError('out_channels%2==1 and gnn_k%2==1')
-        self.gnn1_pool = GNN(1, out_clusters, out_clusters,gnn_k=gnn_k,gnn_type=gnn_type,add_loop=True,jump=jump)
-        self.gnn1_embed = GNN(1, hidden_channels, out_channels,gnn_k=gnn_k,gnn_type=gnn_type,add_loop=True,jump=jump)
+        self.gnn1_pool = GNN(1, out_clusters,gnn_k=gnn_k,gnn_type=gnn_type,add_loop=True,jump=jump)
+        self.gnn1_embed = GNN(1, out_channels,gnn_k=gnn_k,gnn_type=gnn_type,add_loop=True,jump=jump)
 
     def forward(self, x_input, adj, mask=None):
         #print('forward diff')
@@ -157,7 +157,7 @@ class block_adj_pool(torch.nn.Module):
 # a1(x_edge_train,edge_index_train).size()
 # a2=block_edge_conv(num_features,gnn_k=3,gnn_type=2,activation='elu',jump='lstm')
 # a2(x_edge_train,edge_index_train).size()
-# a2(x_weight,x_weight_index,x_weight_weight)
+# a2(x_weight,x_weight_index,x_weight_weight).size()
 # 
 # a3=block_adj_embed(num_features,gnn_k=3,gnn_type=1,activation='leaky',jump='lstm')
 # a3(x_adj,x_adj_adj).size()
@@ -167,6 +167,10 @@ class block_adj_pool(torch.nn.Module):
 # a5=fc_edge(88,[8,1])
 # a5(torch.ones(5,88))
 # =============================================================================
+#edge x.size()=batch*num_fetures,channels
+
+
+#adj x.size()=batch,num_features,n_channels
 
 
 class Net0(torch.nn.Module):#gcn
